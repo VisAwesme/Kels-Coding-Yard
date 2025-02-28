130b36fae53ac955328f9cc20d0708945bd32de8
VisAwesme <104176687+VisAwesme@users.noreply.github.com>
2025-02-28 08:59:41 -0500

    THE VOICES ARE GETTING LOUDER

---
+++ maxwell.py
@@ -0,0 +1,24 @@
+# Made By DrKel
+# Please Credit DrKel if reusing in your own scripts (Or VisAwesme or Vaughn R)... not sure why you would wana reuse this script though...
+# I love making these types of scripts omfg its not even funny anymore
+
+import webbrowser
+
+def meow_function_one():
+    print("meow")
+
+def meow_function_two():
+    print("meow meow")
+
+def meow_function_three():
+    print("meow meow meow")
+
+def main():
+    meow_function_one()
+    meow_function_two()
+    meow_function_three()
+    # Redirect to the YouTube video
+    webbrowser.open("https://www.youtube.com/watch?v=YRvOePz2OqQ")
+
+if __name__ == "__main__":
+    main()
